ffe284ed7f4db674c48f68541e7ce481bb4b6af9
Tarun Gautam <tarungautam.work@gmail.com>
2026-03-04 11:26:28 -0800

    chore: remove backend and local config files

--- server/index.ts
+++
@@ -1,8 +0,0 @@
-import { app } from './app.ts'
-import { getServerEnv } from './env.ts'
-
-const { PORT } = getServerEnv()
-
-app.listen(PORT, () => {
-  console.log(`HICH server listening on port ${PORT}`)
-})
